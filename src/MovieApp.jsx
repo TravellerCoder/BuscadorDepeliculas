716b76c5cb63f835170b8b35d4cbe9a7d77cbfd6
Traveller Coder <guillermobukowski@gmail.com>
2025-04-08 11:59:10 -0300

    primer commit

---
+++ src/MovieApp.jsx
@@ -0,0 +1,15 @@
+import './MovieApp.css'
+
+
+export const MovieApp = () => {
+  return (
+    <div className='container'>
+        <h1 className='title'>Movie App</h1>
+
+        <form>
+            <input type="text" placeholder='Busca tu pelicula' className='input'/>
+            <button className='button'>Buscar</button>
+        </form>
+    </div>
+  )
+}
